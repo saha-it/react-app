0686412d776d44c7308ae6fab44f403edf464a37
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-05 11:43:33 +0900

    ヘッダー実装

---
+++ app/src/component/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="bg-[#fff] h-12 w-full absolute top-0">
+            <div>
+                <p className="">bar</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
