dcb34ea6e84a487d5adc679656442ea3ede291f3
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-06 16:55:56 +0900

    猫の画像を中央に（レスポンシブ対応）

--- app/src/component/Top.tsx
+++ app/src/component/Top.tsx
@@ -11,7 +11,10 @@ function Top() {
                         className="rounded-[600px] opacity-75"
                         src={barCounter}
                     ></img>
-                    <img className="absolute top-20 left-80" src={cat}></img>
+                    <img
+                        className="absolute top-0 left-0 bottom-0 right-0 m-auto"
+                        src={cat}
+                    ></img>
                 </div>
             </div>
         </div>
