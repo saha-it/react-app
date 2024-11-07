4adb987e9d3ac4b3b19bc3d2a92384d5086cfead
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-07 16:36:24 +0900

    表示部分の修正

--- app/src/component/SearchResult.tsx
+++ app/src/component/SearchResult.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import commodity from "../data/commodity.json";
 import axios from "axios";
 
-/* import SelectAlcohol from "./SelectAlcohol"; */
-import SelectType from "./SelectType";
-/* import SelectTest from "./SelectTest"; */
+import AlcoholCard from "./AlcoholCard";
 
-import SelectAlcohol from "./SelectAlcohol";
+import SelectType from "./SelectType";
+import SelectAlcoholContent from "./SelectAlcoholContent";
+import SelectCompany from "./SelectCompany";
+import SelectFlavor from "./SelectFlavor";
 
 interface Item {
     itemName: string;
@@ -19,6 +20,12 @@ interface Object {
     itemCode: string;
 }
 
+interface Items {
+    Item: Item;
+    itemName: string;
+    itemCode: string;
+}
+
 //API叩いて対象商品を全件取得
 let allDatas: Array<Object> = [];
 //"https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?format=json&genreId=408186&shopCode=kuranosuke&maxPrice=500&page=2&applicationId=1075902594404183588"
@@ -26,25 +33,8 @@ let allDatas: Array<Object> = [];
 const url =
     "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?format=json&genreId=408186&applicationId=1075902594404183588";
 
-/* function getAll(page: number) {
-    axios
-        .get(
-            "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?format=json&genreId=408186&shopCode=kuranosuke&maxPrice=500&page=" +
-                page +
-                "&applicationId=1075902594404183588"
-        )
-        .then((res) => {
-            const items = res.data.Items;
-            allDatas.push(...items);
-            console.log(allDatas);
-            console.log("OK");
-        })
-        .catch((error) => {
-            console.log("失敗");
-            console.log(error.status);
-        });
-}
-*/
+const affiliateId = "41388598.82f92f73.41388599.896fa6e2";
+const applicationId = "1075902594404183588";
 
 let counter = 1;
 const timer = setInterval(async function () {
@@ -75,10 +65,16 @@ const TestParent: React.FC = () => {
     const [alcohol, setAlcohol] = useState("");
     const [type, setType] = useState("");
     const [datas, setDatas] = useState(Array<Object>);
-    const changeAlcohol = (newValue: string) => {
+    const changeAlcoholContent = (newValue: string) => {
         setAlcohol(newValue);
     };
     const changeType = (newValue: string) => {
+        setType(newValue);
+    };
+    const changeCompany = (newValue: string) => {
+        setType(newValue);
+    };
+    const changeFlavor = (newValue: string) => {
         setType(newValue);
     };
 
@@ -86,8 +82,28 @@ const TestParent: React.FC = () => {
         //最初に配列を空にする
         let arr: Array<Object> = [];
         const list = commodity.commodity.filter(
-            (value) => value.alcohol == alcohol && value.type == type
+            (v) =>
+                //未選択のものは全て取得する処理
+                (alcohol !== ""
+                    ? v.alcoholContent == alcohol
+                    : v.alcoholContent !== undefined) &&
+                (type !== "" ? v.type == type : v.type !== undefined)
         );
+
+        /* let list = commodity.commodity.filter(function (value) {
+            console.log(alcohol);
+            if (alcohol !== "") {
+                return value.alcohol == alcohol;
+            }
+            return value;
+        });
+        list = list.filter(function (value) {
+            if (type !== "") {
+                return value.type == type;
+            }
+            return value;
+        }); */
+
         for await (const value of list) {
             /* await axios
                 .get(
@@ -116,32 +132,23 @@ const TestParent: React.FC = () => {
                 arr.push(drinkList[0]);
             }
         }
-        /*  async function add() {
-            setDatas(arr);
-            console.log(datas);
-        }
-        add(); */
         setDatas(arr);
         /*
             TODO
             undifinedの削除処理と原因究明
         */
-        console.log(arr);
-        console.log(list);
-        //setDatas(arr);
-        //setDatas(arr);
-        //console.log(datas);
     }
 
     return (
         <div className="mt-8">
-            <p className="text-[#fff] w-fit m-auto">
+            <AlcoholCard />
+            <p className="text-[#fff] w-fit m-auto font-serif font-semibold">
                 お客様の好みを教えてください
             </p>
-            {/* <SelectTest onValueChange={changeAlcohol} /> */}
-            {/* <SelectAlcohol onValueChange={changeAlcohol} /> */}
             <SelectType changeType={changeType} />
-            <SelectAlcohol onChangeAlcohol={changeAlcohol} />
+            <SelectAlcoholContent changeAlcoholContent={changeAlcoholContent} />
+            <SelectCompany changeCompany={changeCompany} />
+            <SelectFlavor onChangeFlavor={changeFlavor} />
             <div className="bg-[#fff] w-fit m-auto">
                 <button onClick={() => handleClick()} className="bg-[#fff]">
                     Click me
@@ -152,7 +159,6 @@ const TestParent: React.FC = () => {
                 return (
                     <p key={key} className="bg-[#fff]">
                         {value.Item.itemName}
-                        {key}
                     </p>
                 );
             })}
