d863419adc992f1e2620ba1785c65927bd43cccb
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-12 18:28:33 +0900

    検索機能の実装、調整 コンポーネントの分類 検索条件の表示処理

--- app/src/component/SearchResult.tsx
+++ app/src/component/SearchResult.tsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-import commodity from "../data/commodity.json";
 import axios from "axios";
 
 import SelectType from "./SelectType";
 import SelectAlcoholContent from "./SelectAlcoholContent";
 import SelectCompany from "./SelectCompany";
 import SelectFlavor from "./SelectFlavor";
+import SelectFruit from "./SelectFruit";
 
 import AlcoholCard from "./AlcoholCard";
+
+import GetRequest from "./GetRequest";
+
+import commodity from "../data/commodity.json";
+import Untitled from "../data/Untitled.json";
+
+import imgCatHead from "../images/cat_head.png";
 
 interface Item {
     itemName: string;
@@ -34,18 +41,12 @@ interface Items {
     itemCode: string;
 }
 
-interface essentialData {
-    Item: {
-        company: string;
-        name: string;
-        alcoholContent: string;
-        type: string;
-        flavor: string;
-        itemCode: string;
-        itemName: string;
-        mediumImageUrls: Array<mediumImageUrls>;
-        affiliateUrl: string;
-    };
+interface Condition {
+    alcoholContetnt: string;
+    type: string;
+    company: string;
+    flavor: string;
+    fruitType: string;
 }
 
 //API叩いて対象商品を全件取得
@@ -92,8 +93,9 @@ const TestParent: React.FC = () => {
     const [type, setType] = useState("");
     const [company, setCompany] = useState("");
     const [flavor, setFlavor] = useState("");
+    const [fruit, setFruit] = useState("");
+    const [condition, setCondition] = useState(Object);
     const [datas, setDatas] = useState(Array<Object>);
-    const [essentialDatas, setEssentialData] = useState(Array<essentialData>);
     const changeAlcoholContent = (newValue: string) => {
         setAlcohol(newValue);
     };
@@ -104,30 +106,101 @@ const TestParent: React.FC = () => {
         setCompany(newValue);
     };
     const changeFlavor = (newValue: string) => {
-        setType(newValue);
+        setFlavor(newValue);
+    };
+    const changeFruit = (newValue: string) => {
+        setFruit(newValue);
     };
 
     async function handleClick() {
         //最初に配列を空にする
         let arr: Array<Object> = [];
-        const list = commodity.commodity.filter(
-            (v) =>
-                //未選択のものは全て取得する処理
-                (alcoholContent !== ""
-                    ? v.alcoholContent == alcoholContent
-                    : v.alcoholContent !== undefined) &&
-                (type !== "" ? v.type == type : v.type !== undefined) /* && */
-            /* (company != ""
+        const list = Untitled.filter((v) => {
+            //アルコール度数のレベルを3段階に分ける
+            let alcoholLevel = "";
+            if (v.alcoholContent < 4) {
+                alcoholLevel = "low";
+            } else if (v.alcoholContent >= 4 && v.alcoholContent < 8) {
+                alcoholLevel = "middle";
+            } else if (v.alcoholContent >= 8) {
+                alcoholLevel = "high";
+            }
+            //条件が指定されており、かつ一致しなければ中断
+            if (alcoholContent !== "" && alcoholLevel != alcoholContent) {
+                return;
+            }
+            if (type !== "" && v.type != type) {
+                return;
+            }
+            if (company !== "" && v.company != company) {
+                return;
+            }
+            if (fruit !== "" && v.fruitType != fruit) {
+                return;
+            }
+            //flavorはフラグが0の時falseとなる
+            if (flavor !== "") {
+                switch (flavor) {
+                    case "rich":
+                        if (v.richFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "refreshing":
+                        if (v.refreshingFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "bitter":
+                        if (v.bitterFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "sour":
+                        if (v.sourFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "sweet":
+                        if (v.sweetFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "dry":
+                        if (v.dryFlg == 0) {
+                            return;
+                        }
+                        break;
+                    case "fruity":
+                        if (v.fruityFlg == 0) {
+                            return;
+                        }
+                        break;
+                }
+            }
+            //未選択のものは全て取得する処理
+            /* (alcoholContent !== ""
+                ? alcoholLevel == alcoholContent
+                : v.alcoholContent !== undefined) &&
+            (type !== "" ? v.type == type : v.type !== undefined)
+            &&
+            (company != ""
                     ? v.company == company
                     : v.company != undefined) &&
                 (flavor != "" ? v.flavor == flavor : v.flavor != undefined) */
-        );
+            //console.log(v);
+            //指定の条件に一致したデータのみ返す
+            return v;
+        });
 
         for await (const value of list) {
-            let drinkList: any = allDatas.filter(
-                (drink) => value.itemCode == drink.Item.itemCode
-            );
-            //console.dir(drinkList[0]);
+            let drinkList: any = allDatas.filter((drink, index) => {
+                if (value.itemCode == drink.Item.itemCode) {
+                    console.log(index);
+                }
+
+                return value.itemCode == drink.Item.itemCode;
+            });
             /*
                 TODO 画面表示用にデータを整形
                 商品名、アルコール度数、値段、会社名、種類
@@ -137,115 +210,64 @@ const TestParent: React.FC = () => {
             }
         }
         setDatas(arr);
-    }
 
-    function getJSON() {
-        const list = allDatas.filter((v, index) => {
-            let param = "";
-            //itemNameの最初の部分だけを抜き出す処理
-            for (let i = 0; i < v.Item.itemName.length; i++) {
-                if (i != 0 && v.Item.itemName[i] == "【") {
-                    break;
-                } else {
-                    param += v.Item.itemName[i];
-                }
-            }
-            return (
-                (company != ""
-                    ? v.Item.itemName.includes(company)
-                    : v.Item.itemName != undefined) &&
-                (type !== ""
-                    ? param.includes(type)
-                    : v.Item.itemName != undefined)
-            );
+        //検索条件の保存
+        setCondition({
+            ...condition,
+            alcoholContent: alcoholContent,
+            type: type,
+            company: company,
+            flavor: flavor,
+            fruitType: fruit,
         });
-        let arr: Array<essentialData> = [];
-        let essentialData = list.map((v) => {
-            let obj: essentialData = {
-                Item: {
-                    company: company,
-                    name: "",
-                    alcoholContent: "",
-                    type: type,
-                    flavor: "",
-                    itemCode: v.Item.itemCode,
-                    itemName: v.Item.itemName,
-                    mediumImageUrls: v.Item.mediumImageUrls,
-                    affiliateUrl: v.Item.affiliateUrl,
-                },
-            };
-            arr.push(obj);
-            return obj;
-        });
-        setEssentialData(arr);
-        interface testData {
-            company: string;
-            name: string;
-            alcoholContent: string;
-            type: string;
-            flavor: string;
-            itemCode: string;
-            itemName: string;
-        }
-        let testList = essentialData.map((v) => {
-            let obj: testData = {
-                company: company,
-                name: "",
-                alcoholContent: "",
-                type: type,
-                flavor: "",
-                itemCode: v.Item.itemCode,
-                itemName: v.Item.itemName,
-            };
-            return obj;
-        });
-
-        console.log(list);
-        //console.log(JSON.stringify(essentialData, null, "\t"));
-        console.log(JSON.stringify(testList, null, "\t"));
     }
 
     return (
         <div className="mt-8">
-            <p className="text-[#fff] w-fit m-auto font-serif font-semibold">
-                お客様の好みを教えてください
-            </p>
+            <div className="flex w-fit m-auto">
+                <img
+                    className="w-6 h-6 text-[#fff] color-[#fff]"
+                    src={imgCatHead}
+                />
+                <p className="text-[#fff] w-fit font-serif font-semibold mb-16">
+                    お客様の好みをお伺いします
+                </p>
+                <img
+                    className="w-6 h-6 text-[#fff] color-[#fff]"
+                    src={imgCatHead}
+                />
+            </div>
             <SelectType changeType={changeType} />
             <SelectAlcoholContent changeAlcoholContent={changeAlcoholContent} />
             <SelectCompany changeCompany={changeCompany} />
             <SelectFlavor onChangeFlavor={changeFlavor} />
-            <div className="bg-[#fff] w-fit m-auto">
-                <button onClick={() => handleClick()} className="bg-[#fff] p-2">
+            <SelectFruit changeFruit={changeFruit} />
+            <div className="bg-[#fff] w-fit m-auto mb-16">
+                <button
+                    onClick={() => handleClick()}
+                    className="font-serif font-semibold bg-[#fff] p-2 border-4 border-indigo-200 border-b-indigo-500 border-r-indigo-500"
+                >
                     商品を探す
                 </button>
             </div>
-            <div className="bg-[#fff] w-fit m-auto">
-                <button onClick={() => getJSON()} className="bg-[#fff] p-2">
-                    JSON取得
-                </button>
+
+            <div className="text-[#fff] font-serif font-semibold w-fit m-auto">
+                <p>お酒の種類:{condition.type}</p>
+                <p>アルコールレベル:{condition.alcoholContent}</p>
+                <p>メーカー:{condition.company}</p>
+                <p>味わい:{condition.flavor}</p>
+                <p>フルーツ:{condition.fruit}</p>
             </div>
 
-            <div className="flex m-auto w-fit">
+            {/*requestを取得するだけのコンポーネント */}
+            {/* <GetRequest company={company} type={type} /> */}
+
+            <div className="flex m-auto w-[90vw] flex-wrap">
                 {datas.map((value, key) => {
                     return (
                         <AlcoholCard
                             key={key}
                             imageUrl={value.Item.mediumImageUrls[0].imageUrl}
-                            /* company={value.Item.itemName} */
-                            itemName={value.Item.itemName}
-                            affiliateUrl={value.Item.affiliateUrl}
-                        />
-                    );
-                })}
-            </div>
-
-            <div className="flex m-auto w-[90vw] flex-wrap">
-                {essentialDatas.map((value, key) => {
-                    return (
-                        <AlcoholCard
-                            key={key}
-                            imageUrl={value.Item.mediumImageUrls[0].imageUrl}
-                            /* company={value.Item.itemName} */
                             itemName={value.Item.itemName}
                             affiliateUrl={value.Item.affiliateUrl}
                         />
