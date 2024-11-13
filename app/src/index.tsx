c8893f2391fc0231c5e54cdec5c0be7e4d422645
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-13 16:30:10 +0900

    不要ヵ所の削除、非表示処理 トップに戻るボタンの調整

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Header from "./component/Header";
 import Top from "./component/Top";
 import SearchResult from "./component/SearchResult";
-
 import imgDoor from "./images/door1.png";
-
-import SelectTest from "./component/SelectTest";
 import "./App.css";
 
 const root = ReactDOM.createRoot(
@@ -22,21 +18,14 @@ const APP_NAME: String = "react-app";
 root.render(
     <div>
         <a
-            className="fixed z-10 right-10 bottom-10 bg-[#fff] w-24 h-24 rounded-full text-center"
+            className="fixed z-10 right-10 bottom-10 bg-[#fff] w-28 h-28 rounded-full text-center"
             href="#top"
         >
-            <img className="w-20 m-auto px-4 pt-4" src={imgDoor}></img>
-            <p className="text-xs">TOPにもどる</p>
+            <img className="w-20 m-auto px-4 pt-6" src={imgDoor}></img>
+            <p className="text-xs font-serif font-semibold">トップにもどる</p>
         </a>
-
-        {/* <BrowserRouter>
-            <Routes>
-                <Route path={APP_NAME + "/"} element={<App />} />
-                <Route path={APP_NAME + "/top"} element={<Top />} />
-            </Routes>
-        </BrowserRouter> */}
         {/* <Search /> */}
-        <Header />
+        {/* <Header /> */}
         <Top />
         <SearchResult />
     </div>
