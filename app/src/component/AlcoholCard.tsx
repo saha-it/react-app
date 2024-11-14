6cb53c0ccf95f157f168eda4342eb9bba9737234
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-11-14 10:44:42 +0900

    画像の背景に白い円を追加

--- app/src/component/AlcoholCard.tsx
+++ app/src/component/AlcoholCard.tsx
@@ -3,8 +3,12 @@ import React from "react";
 const AlcoholCard = (prop: any) => {
     return (
         <div className="w-[30vw] max-md:w-[40vw] max-sm:w-[80vw] border-4 border-red-500 bg-[#F2E5BF]">
-            <div>
-                <img className="m-auto w-fit" src={prop.imageUrl}></img>
+            <div className="relative h-[16rem]">
+                <div className="absolute w-[200px] h-[200px] m-auto top-0 left-0 right-0 bottom-0 border-4 bg-[#fff] rounded-full"></div>
+                <img
+                    className="absolute m-auto w-fit z-10 top-0 left-0 right-0 bottom-0"
+                    src={prop.imageUrl}
+                ></img>
             </div>
             <div className="text-[#803D3B] font-serif font-semibold text-center">
                 <p>{prop.company}</p>
@@ -15,7 +19,7 @@ const AlcoholCard = (prop: any) => {
                     href={prop.affiliateUrl}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[#fff]"
+                    className="text-[#fff] font-serif font-semibold"
                 >
                     楽天市場で商品を見る
                 </a>
